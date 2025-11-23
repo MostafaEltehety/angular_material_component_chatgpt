02675738aba839da8def88a4d15f2ab1715d0b2f
mostafa.eltehety@gmail.com <mostafa.eltehety@gmail.com>
2025-11-23 06:12:38 +0200

    3

--- src/app/components/select/select.ts
+++ src/app/components/select/select.ts
@@ -1,24 +1,69 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import{MatSelectModule}from '@angular/material/select';
-import { FormField } from "../form-field/form-field";
+import { Component, OnInit } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+} from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { FormField } from '../form-field/form-field';
+import { MatInput } from "@angular/material/input";
+import { MatFormFieldModule } from '@angular/material/form-field';
 @Component({
   selector: 'app-select',
-  imports: [MatSelectModule,ReactiveFormsModule, FormsModule, FormField],
+  imports: [MatSelectModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatInput,
+    MatFormFieldModule, FormField],
   templateUrl: './select.html',
   styleUrl: './select.scss',
 })
-export class Select {
-country:string='';
-form=new FormGroup({
-  city:new FormControl('')
-});
+export class Select implements OnInit {
+  ngOnInit(): void {
+    this.filteredEmployees = this.employees;
+  }
 
-cities:string[]=['Assuit','Cairo','Gizaa']
+  country: string = '';
+  form = new FormGroup({
+    city: new FormControl(''),
+  });
 
-skills:string[]=['C#',"Angular","SQL Server"];
+  cities: string[] = ['Assuit', 'Cairo', 'Gizaa'];
 
-selectSkills:string[]=[];
+  skills: string[] = [
+    'C#',
+    'Python',
+    'TypeScript',
+    'JavaScript',
+    'React',
+    'Vue',
+    'Angular',
+    'SQL Server',
+  ];
+
+  selectSkills: string[] = [];
+
+  employees: string[] = [
+    'Mostafa',
+    'Ahmed',
+    'Mohamed',
+    'Ali',
+    'Heba',
+    'Khalid',
+    'Omar',
+    'Yassmin',
+    'Esraa',
+  ];
+  textSerach = '';
+  selectedEmp = '';
+  filteredEmployees: string[] = [];
+
+  filterEmployees() {
+    this.filteredEmployees = this.employees.filter((emp) =>
+      emp.toLowerCase().includes(this.textSerach.toLowerCase())
+    );
+  }
 
 
 }
